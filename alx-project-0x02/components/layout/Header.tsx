9d66b7d899085b738c881edb4486aacd7e988b56
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-10 22:06:58 +0700

    implementing page routing to home and about pages

--- alx-project-0x02/components/layout/Header.tsx
+++ alx-project-0x02/components/layout/Header.tsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
 const Header = () => {
   return (
     <div>
       <h1>Header Section</h1>
+      <ul>
+        <li>
+        <a href="/">Home</a>
+        </li>
+        <li>
+        <a href="/about">About</a>
+        </li>
+        
+      </ul>
     </div>
   )
 }
--- alx-project-0x02/pages/index.tsx
+++ alx-project-0x02/pages/index.tsx
@@ -1,8 +1,10 @@
+import Header from "@/components/layout/Header";
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
+      <Header />
     </div>
   );
 }
