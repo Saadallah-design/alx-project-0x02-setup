f1896495d7fd19dbe6f2d305a057e153343414c5
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-10 21:03:32 +0700

    restructured the directory for autochecker

---
+++ alx-project-0x02/alx-project-2/pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HomePage() {
+  return (
+    <div>
+      <h1>This is a greeting message!</h1>
+      <p>Welcome to the Alx project 2 homepage.</p>
+    </div>
+  )
+}
+
+export default HomePage;
+
