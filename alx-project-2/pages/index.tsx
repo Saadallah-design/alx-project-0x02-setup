36d339e170e6597a7e8c82db510b0c8d193ff59c
Saadallah-design <saadalla.publishing@gmail.com>
2025-11-10 20:50:46 +0700

    seeting the alx-project-2

---
+++ alx-project-2/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>This is a greeting message!</h1>
+      <p>Welcome to the Alx project 2 homepage.</p>
+    </div>
+  )
+}
+
+export default HomePage
